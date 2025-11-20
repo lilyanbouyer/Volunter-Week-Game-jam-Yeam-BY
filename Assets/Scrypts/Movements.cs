c0c8b5f8a912709341e367437dd31b8c0dc079d3
lili boubou <lilyan.bouyer@ynov.com>
2025-11-20 15:03:35 +0100

    add movement

--- Assets/Scrypts/Movements.cs
+++ Assets/Scrypts/Movements.cs
@@ -1,16 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    public float walkingSpeed = 7.5f;
+    public float runningSpeed = 11.5f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public Camera playerCamera;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 45.0f;
+
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+    [HideInInspector] public bool canMove = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        // Verrouille le curseur
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Calcule les directions avant/arrière et gauche/droite
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        // Détermine la vitesse de déplacement en fonction de la touche Shift
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+
+        // Calcule la direction de déplacement
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        // Gère le saut
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded) {
+            moveDirection.y = jumpSpeed;
+        } else {
+            moveDirection.y = moveDirection.y;
+        }
+
+        // Applique la gravité si le joueur n'est pas au sol
+        if (!characterController.isGrounded) {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        // Applique le mouvement
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        // Gère la rotation de la caméra (haut/bas et gauche/droite)
+        if (canMove) {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
     }
 }
